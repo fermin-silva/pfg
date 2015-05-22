c2c038ea7722b890d96b5d44729d31571d9632ab
Fermin Silva <silvafermn@gmail.com>
2015-05-22 14:18:53 -0300

    Mejoras en la interfaz web

--- web/assets/js/bootswatch.js
+++ web/assets/js/bootswatch.js
@@ -24,6 +24,9 @@
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
 
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+
   $(".bs-component").hover(function(){
     $(this).append($button);
     $button.show();
